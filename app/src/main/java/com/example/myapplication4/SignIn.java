55284417aa59cc1c84b95c4c251c95ca08dd5db6
Sharanya Pathakota <100440931+sharanyap21@users.noreply.github.com>
2024-04-16 01:13:08 -0400

    adding navigation

--- app/src/main/java/com/example/myapplication4/SignIn.java
+++ app/src/main/java/com/example/myapplication4/SignIn.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
