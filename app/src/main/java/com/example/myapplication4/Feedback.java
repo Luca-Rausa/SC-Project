ea6bcc0b68afa47f92be9d88a5263b839d429f29
Sharanya Pathakota <100440931+sharanyap21@users.noreply.github.com>
2024-04-25 17:57:00 -0400

    help

--- app/src/main/java/com/example/myapplication4/Feedback.java
+++ app/src/main/java/com/example/myapplication4/Feedback.java
@@ -1,13 +1,50 @@
 package com.example.myapplication4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Feedback extends AppCompatActivity {
+
+    private Button signUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feedback);
+
+        signUpButton = findViewById(R.id.nextbtn);
+
+        // Setting up the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Enabling the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Feedback.this, Home.class));
+                finish();
+            }
+        });
+    }
+
+    // Handle toolbar item clicks
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Handle back button click
+            startActivity(new Intent(Feedback.this, Home.class));
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
