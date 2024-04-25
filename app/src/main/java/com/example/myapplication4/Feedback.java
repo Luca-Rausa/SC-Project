f14b487c187e07ee1aea03049446905db224cc77
Sharanya Pathakota <100440931+sharanyap21@users.noreply.github.com>
2024-04-25 14:32:28 -0400

    one working product

--- app/src/main/java/com/example/myapplication4/Feedback.java
+++ app/src/main/java/com/example/myapplication4/Feedback.java
@@ -1,4 +1,13 @@
 package com.example.myapplication4;
 
-public class Feedback {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Feedback extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feedback);
+    }
 }
